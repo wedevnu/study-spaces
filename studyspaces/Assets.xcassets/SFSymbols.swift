c8713ff889e72d2a4838ef18683b5c7925be5080
bella chin <bellachin08@gmail.com>
2024-11-12 23:16:18 -0500

    added star SFSymbols
    fix merge conflict

---
+++ studyspaces/Assets.xcassets/SFSymbols.swift
@@ -0,0 +1,11 @@
+//
+//  SFSymbols.swift
+//  studyspaces
+//
+//  Created by bella chin on 11/12/24.
+//
+
+struct SFSymbols {
+    static let emptyStar: String = "star"
+    static let filledStar: String = "star.fill"
+}
